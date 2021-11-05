b19263e2c6f3f3ba499abca15ac6e27f21bdcbde
manolobkno08 <manologm10@hotmail.com>
2021-11-05 11:44:33 -0500

    add task 7

--- console.py
+++ console.py
@@ -1,49 +1,50 @@
 #!/usr/bin/python3
 
 import cmd
-import json
 from models.base_model import BaseModel
 
 """
 Admin Console
 
 """
-classes = {
-    "BaseModel": BaseModel
-}
 
 
 class HBNBCommand(cmd.Cmd):
     """Interpreter"""
     prompt = "(hbnb) "
 
+    classes = {
+        "BaseModel": BaseModel
+    }
+
     def do_create(self, args):
         """Create instances"""
         args = args.split(' ')
-        if len(args) == 0:
+        if len(args[0]) == 0:
             print("** class name missing **")
-        if args not in HBNBCommand.classes:
+        elif args[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
-        instance = eval(args)()
-        instance.save()
-        print(instance.id)
-
-    def do_show(self, args):
-        """Print instances"""
-        args = args.split(' ')
-        if len(args) == 0:
-            print("** class name missing **")
-        if args in HBNBCommand.classes:
-            if len(args) > 1:
-                key = args[0] + "." + args[1]
-                if key in models.storage.all():
-                    print(models.storage.all()[key])
-                else:
-                    print("** no instance found **")
-            else:
-                print("** instance id missing **")
         else:
-            print("** class doesn't exist **")
+            instance = BaseModel()
+            instance.save()
+            print(instance.id)
+
+    # def do_show(self, args):
+    #     """Print instances"""
+    #     args = args.split(' ')
+    #     if len(args) == 0:
+    #         print("** class name missing **")
+    #     if args in HBNBCommand.classes:
+    #         if len(args) > 1:
+    #             key = args[0] + "." + args[1]
+    #             if key in models.storage.all():
+    #                 print(models.storage.all()[key])
+    #             else:
+    #                 print("** no instance found **")
+    #         else:
+    #             print("** instance id missing **")
+    #     else:
+    #         print("** class doesn't exist **")
 
     def do_EOF(self, args):
         """Quit command to exit the program
