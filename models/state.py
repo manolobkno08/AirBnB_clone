cfed2b0b8ccb2930f8f5a6596d05008c9077b6fe
SandraHoyos <slhoyosr@gmail.com>
2021-11-06 17:39:51 -0500

    state

---
+++ models/state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""module inherits from baseModel class"""
+
+from models.base_model import BaseModel
+
+
+class state(baseModel):
+    """Represent a state"""
+
+    name = ""
