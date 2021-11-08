5f469e9fae41f8faee8a56addd3f199e71606475
manolobkno08 <manologm10@hotmail.com>
2021-11-07 22:34:08 -0500

    test user

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Unittest for User
+"""
+import unittest
+from models.user import User
+base_model = __import__('user').__init__
+
+
+class Testinit(unittest.TestCase):
+    """
+    User TestCase
+    """
