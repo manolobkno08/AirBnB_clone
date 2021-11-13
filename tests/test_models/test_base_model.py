8f03ef5802781552e7b4b0e25e9f47b2ac4c7978
manolobkno08 <manologm10@hotmail.com>
2021-11-12 22:25:19 -0500

    add uniest bm

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,14 +3,28 @@
 Unittest for base_model
 """
 import unittest
+from datetime import datetime
+from unittest import mock
+import pycodestyle
+import time
+from unittest.mock import MagicMock
 from models.base_model import BaseModel
-base_model = __import__('base_model').__init__
+from models.base_model import __doc__
+
+# base_model = __import__('base_model').__init__
 
 
-class Testinit(unittest.TestCase):
+class Test_pycodestyle(unittest.TestCase):
     """
     base_model TestCase
     """
+
+    def test_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pycodestyle.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/base_model.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
 
 if __name__ == '__main__':
